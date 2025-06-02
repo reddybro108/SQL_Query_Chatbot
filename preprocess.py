273ba8d36e978d609cae954777b868582a7960cf
reddybro108 <mail2amolreddy@gmail.com>
2025-06-02 14:33:25 +0530

    Update CI/CD workflow and confirm YAML syntax

--- preprocess.py
+++ preprocess.py
@@ -6,6 +6,7 @@ from nltk.corpus import stopwords
 nltk_data_path = os.path.join(os.path.dirname(__file__), 'nltk_data')
 nltk.data.path.append(nltk_data_path)
 
+
 def download_nltk_data():
     try:
         nltk.data.find('tokenizers/punkt')
@@ -20,42 +21,9 @@ def download_nltk_data():
     except LookupError:
         nltk.download('stopwords', download_dir=nltk_data_path)
 
-download_nltk_data()
 
 def extract_keywords(text):
     tokens = word_tokenize(text.lower())
     stop_words = set(stopwords.words('english'))
     keywords = [word for word in tokens if word not in stop_words and word.isalnum()]
     return keywords
-
-
-
-
-# import nltk
-# import os
-# from nltk.tokenize import word_tokenize
-# from nltk.corpus import stopwords
-
-# nltk_data_path = os.path.join(os.path.dirname(__file__), "nltk_data")
-# nltk.data.path.append(nltk_data_path)
-
-
-# def download_nltk_data():
-#     try:
-#         nltk.data.find("tokenizers/punkt")
-#     except LookupError:
-#         nltk.download("punkt", download_dir=nltk_data_path)
-#     try:
-#         nltk.data.find("corpora/stopwords")
-#     except LookupError:
-#         nltk.download("stopwords", download_dir=nltk_data_path)
-
-
-# download_nltk_data()
-
-
-# def extract_keywords(text):
-#     tokens = word_tokenize(text.lower())
-#     stop_words = set(stopwords.words("english"))
-#     keywords = [word for word in tokens if word not in stop_words and word.isalnum()]
-#     return keywords
