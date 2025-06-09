bb15e8385ae990703e16211a2021bd3cdef0698b
reddybro108 <mail2amolreddy@gmail.com>
2025-06-09 15:39:50 +0530

    code modified3

--- app/database.py
+++ app/database.py
@@ -18,7 +18,7 @@ def get_connection() -> pymysql.connections.Connection:
         "host": os.getenv("DB_HOST", "localhost"),
         "port": int(os.getenv("DB_PORT", 3306)),
         "user": os.getenv("DB_USER", "root"),
-        "password": os.getenv("DB_PASSWORD", "yourpassword"),
+        "password": os.getenv("DB_PASSWORD", "Amolreddy@108"),
         "database": os.getenv("DB_NAME", "amoldb"),
         "charset": "utf8mb4",
         "cursorclass": DictCursor,
